86e4862a8f5c96f65ee932442d766f29d80ca641
SmKou <66178856+SmKou@users.noreply.github.com>
2023-07-31 13:53:12 -0700

    setup DoctorOffice

---
+++ DoctorOffice/Program.cs
@@ -0,0 +1,37 @@
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using DoctorOffice.Models;
+
+namespace DoctorOffice;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllersWithViews();
+        builder.Services.AddDbContext<DoctorOfficeContext>(
+            dbContextOptions => dbContextOptions.UseMySql(
+                builder.Configuration["ConnectionStrings:DefaultConnection"],
+                ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"])
+            )
+        );
+        WebApplication app = builder.Build();
+
+        // app.UseDeveloperExceptionPage();
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}"
+        );
+        app.Run();
+    }
+}
