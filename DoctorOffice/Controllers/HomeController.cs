7990fe797f54268d58e6b2e2a3048abb3f10b3a8
SmKou <66178856+SmKou@users.noreply.github.com>
2023-08-03 02:07:28 -0700

    Add index for home (appointments)

---
+++ DoctorOffice/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+namespace DoctorOffice.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly DoctorOfficeContext _db;
+
+    public HomeController(DoctorOfficeContext db)
+    {
+        _db = db;
+    }
+
+    public ActionResult Index()
+    {
+        List<Appointment> model = _db.Appointments
+            .Include(appointment => appointment.Doctor)
+            .Include(appointment => appointment.Patient)
+            .Include(appointment => appointment.Location)
+            .ToList();
+        return View(model);
+    }
+}
